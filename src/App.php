9bf23bf49a3a7e098fffd17d334ef773973d3f19
johnstevenson <john-stevenson@blueyonder.co.uk>
2016-12-19 20:58:14 +0000

    Add appveyor.yml

---
+++ src/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Choco;
+
+class App
+{
+    public function run()
+    {
+        echo 'PHP version: ',  PHP_VERSION, PHP_EOL;
+    }
+}
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$app = new Choco\App();
+$app->run();
