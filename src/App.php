4560ec3999a6b6873208fae6bd7bca23f1928208
johnstevenson <john-stevenson@blueyonder.co.uk>
2019-11-07 21:38:51 +0000

    Show COMPOSER_HOME location

--- src/App.php
+++ src/App.php
@@ -17,6 +17,7 @@ class App
 
         echo 'PHP binary: ', $binary, PHP_EOL;
         echo passthru('composer --version');
+        echo 'COMPOSER_HOME: ', getenv('COMPOSER_HOME'), PHP_EOL;
         echo 'composer cache dir: ', exec('composer config cache-dir'), PHP_EOL;
         echo 'composer data dir: ', exec('composer config data-dir'), PHP_EOL;
         echo PHP_EOL;
