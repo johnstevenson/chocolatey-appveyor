40f5dd6c3acaaefdee4cef30324c38a0fc6cfafd
johnstevenson <john-stevenson@blueyonder.co.uk>
2019-11-08 13:39:31 +0000

    Show phpci environment variables

--- src/App.php
+++ src/App.php
@@ -7,6 +7,8 @@ class App
     public function run()
     {
         echo PHP_EOL;
+        $this->showPhpCiEnvironment();
+
         $this->output('PHP version', PHP_VERSION);
 
         if (defined('PHP_BINARY')) {
@@ -24,19 +26,7 @@ class App
         $this->output('composer data dir', exec('composer config data-dir'));
         $this->output('composer cache dir', exec('composer config cache-dir'), true);
 
-        if (PHP_VERSION_ID >= 70100) {
-            $env = getenv();
-        } else {
-            $env = array('APPVEYOR_BUILD_FOLDER' => getenv('APPVEYOR_BUILD_FOLDER'));
-        }
-
-        foreach (getenv() as $name => $value) {
-            if (stripos($name, 'APPVEYOR') === 0) {
-                $this->output($name, $value);
-            }
-        }
-
-        echo PHP_EOL;
+        $this->showAppveyorEnvironment();
     }
 
     private function output($name, $value, $break = false)
@@ -44,5 +34,46 @@ class App
         $format = $name !== null ? '%s: %s%s' : '%s%s%s';
         $eol = PHP_EOL . ($break ? PHP_EOL : '');
         printf($format, $name, trim($value), $eol);
+    }
+
+    private function showAppveyorEnvironment()
+    {
+        if (PHP_VERSION_ID >= 70100) {
+            $envars = getenv();
+        } else {
+            $envars = array('APPVEYOR_BUILD_FOLDER' => getenv('APPVEYOR_BUILD_FOLDER'));
+        }
+
+        $env = array();
+
+        foreach ($envars as $name => $value) {
+            if (stripos($name, 'APPVEYOR') === 0) {
+                $env[$name] = $value;
+            }
+        }
+
+        $this->outputEnvironment($env);
+    }
+
+    private function showPhpCiEnvironment()
+    {
+        $names = array('PHPCI_CACHE', 'PHPCI_COMPOSER', 'PHPCI_PHP', 'PHPCI_CHOCO_VERSION');
+        $env = array();
+
+        foreach ($names as $name) {
+            $env[$name] = getenv($name);
+        }
+        $this->outputEnvironment($env);
+    }
+
+    private function outputEnvironment(array $env)
+    {
+        foreach ($env as $name => $value) {
+            $this->output($name, $value);
+        }
+
+        if (!empty($env)) {
+            echo PHP_EOL;
+        }
     }
 }
