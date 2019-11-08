dd639fd0d422f43eff5fccb55d797806945177c9
johnstevenson <john-stevenson@blueyonder.co.uk>
2019-11-08 10:57:53 +0000

    Add missing whitespace

--- src/App.php
+++ src/App.php
@@ -17,11 +17,11 @@ class App
 
         echo 'PHP binary: ', $binary, PHP_EOL;
         echo passthru('composer --version');
-        echo 'composer.phar:', passthru('where composer.phar'), PHP_EOL;
+        echo 'composer.phar: ', passthru('where composer.phar'), PHP_EOL;
         echo 'COMPOSER_HOME: ', getenv('COMPOSER_HOME'), PHP_EOL;
         echo 'composer cache dir: ', exec('composer config cache-dir'), PHP_EOL;
         echo 'composer data dir: ', exec('composer config data-dir'), PHP_EOL;
-        echo 'APPVEYOR_BUILD_FOLDER:', getenv('APPVEYOR_BUILD_FOLDER'), PHP_EOL;
+        echo 'APPVEYOR_BUILD_FOLDER: ', getenv('APPVEYOR_BUILD_FOLDER'), PHP_EOL;
         echo PHP_EOL;
     }
 }
